7b0fd2b48ca9611ea9511f8bbecfaa6dbc626507
李善鹏 <1117@forex.com.cn>
2022-05-29 18:41:20 +0800

    update

---
+++ .history/H5/js/common_20220529151240.js
@@ -0,0 +1,15 @@
+//页面适配
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+
+            document.querySelectorAll('.bg')[0].style.display = 'block'
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
---
+++ .history/H5/js/common_20220529152935.js
@@ -0,0 +1,18 @@
+//页面适配
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+
+            setTimeout(function () { 
+                document.querySelectorAll('.bg')[0].style.opacity = 1
+            },500)
+            
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
