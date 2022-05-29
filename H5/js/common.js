21b7b700223cc5c11d231882489e3d11b40d0962
李善鹏 <1117@forex.com.cn>
2022-05-29 15:29:55 +0800

    upate

--- H5/js/common.js
+++ H5/js/common.js
@@ -7,7 +7,10 @@
             if (!clientWidth) return;
             docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
 
-            document.querySelectorAll('.bg')[0].style.opacity = 1
+            setTimeout(function () { 
+                document.querySelectorAll('.bg')[0].style.opacity = 1
+            },500)
+            
         };
     if (!doc.addEventListener) return;
     win.addEventListener(resizeEvt, recalc, false);
